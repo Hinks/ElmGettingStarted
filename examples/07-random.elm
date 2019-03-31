1f26558e69d3e3e9bbad0188f30f388c67337877
Hinks <henrik.nr10@gmail.com>
2019-03-31 21:07:13 +0200

    added elm-random lib. Drawing the die faces using the svg lib.

--- examples/07-random.elm
+++ examples/07-random.elm
@@ -1,9 +1,10 @@
 import Browser
-import Html exposing (..)
-import Html.Events exposing (..)
-import Html.Attributes exposing (..)
 import Random
 import Dict exposing (Dict)
+import Html
+import Html.Events
+import Svg exposing (..)
+import Svg.Attributes exposing (..)
 
 
 -- MAIN
@@ -29,7 +30,7 @@ type alias Model =
 
 init : () -> (Model, Cmd Msg)
 init _ =
-  ( Model 1
+  ( Model 6
   , Cmd.none
   )
 
@@ -70,14 +71,69 @@ subscriptions model =
 -- VIEW
 
 
-view : Model -> Html Msg
+view : Model -> Html.Html Msg
 view model =
-  let 
-    dieFaceImgPath = "./images/dieface" ++ String.fromInt model.dieFace ++ ".jpeg"
-  in           
-    div []
-      [ 
-        img [ src dieFaceImgPath] []
-      , div [] []
-      , button [ onClick Roll ] [ text "Roll" ]
-      ] 
+  Html.div []
+  [
+    svg [ width "120", height "120", viewBox "0 0 120 120" ] (drawDice model)
+    , Html.div [] []
+    , Html.button [Html.Events.onClick Roll] [Html.text "Roll"]
+
+
+  ]
+  
+drawDice : Model -> List (Svg.Svg msg)
+drawDice model =
+    List.concat [ [roundRect], (drawDieFace model.dieFace) ]
+
+drawDieFace : Int -> List (Svg.Svg msg)
+drawDieFace dieFace = 
+  case dieFace of
+      1 ->
+          [ circle [ cx "60", cy "60", r "10" ] [] ]
+      2 ->
+          [ 
+            circle [ cx "45", cy "60", r "10" ] []
+          , circle [ cx "75", cy "60", r "10" ] [] 
+          ]
+      3 ->
+          [ 
+            circle [ cx "30", cy "90", r "10" ] []
+          , circle [ cx "60", cy "60", r "10" ] []
+          , circle [ cx "90", cy "30", r "10" ] [] 
+          ]
+
+      4 -> 
+          [ 
+            circle [ cx "40", cy "40", r "10" ] [] 
+          , circle [ cx "80", cy "40", r "10" ] []
+          , circle [ cx "40", cy "80", r "10" ] []
+          , circle [ cx "80", cy "80", r "10" ] []
+          ]
+      5 -> 
+          [ 
+            circle [ cx "40", cy "40", r "10" ] [] 
+          , circle [ cx "80", cy "40", r "10" ] []
+          , circle [ cx "60", cy "60", r "10" ] []
+          , circle [ cx "40", cy "80", r "10" ] []
+          , circle [ cx "80", cy "80", r "10" ] []
+          ]
+      _ ->
+          [ 
+            circle [ cx "45", cy "30", r "10" ] [] 
+          , circle [ cx "45", cy "60", r "10" ] [] 
+          , circle [ cx "45", cy "90", r "10" ] [] 
+          , circle [ cx "75", cy "30", r "10" ] [] 
+          , circle [ cx "75", cy "60", r "10" ] [] 
+          , circle [ cx "75", cy "90", r "10" ] [] 
+          ]         
+
+
+
+roundRect : Svg.Svg msg
+roundRect =
+  rect [ x "10", y "10", width "100", height "100", rx "15", ry "15", roundRectStyle] [] 
+
+roundRectStyle : Svg.Attribute msg
+roundRectStyle = 
+  Svg.Attributes.style "fill:lightgrey;stroke:black;stroke-width:2;opacity:0.5"
